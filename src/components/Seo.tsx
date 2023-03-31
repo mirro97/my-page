8d16d06b34641bc85ffdbf34b26febd0b9e176cc
sujeong-k <45249021+mirro97@users.noreply.github.com>
2023-03-31 13:58:07 +0900

    Seo 추가

---
+++ src/components/Seo.tsx
@@ -0,0 +1,44 @@
+import Head from "next/head";
+
+export default function Seo() {
+  return (
+    <Head>
+      <title>프론트엔드 개발자 강수정입니다.</title>
+      <meta charSet="UTF-8" />
+      <meta charSet="euc-kr" />
+      <meta httpEquiv="Content-Type" content="text/html;charset=utf-8" />
+      <meta httpEquiv="Content-Type" content="text/html;charset=euc-kr;" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1;" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="title" content="Resume Sujeong.K" />
+      <meta name="x-apple-disable-message-reformatting" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="author" content="강수정" />
+      <meta
+        name="description"
+        content="👋 안녕하세요, 프론트엔드 개발자 강수정입니다 :)"
+      />
+      <meta
+        name="keywords"
+        content="이력서, 포트폴리오, 개발자 포트폴리오, 프론트엔드개발, React.js, Next.js, 경력기술서, 개인페이지, 프론트엔드, Frontend"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Resume Sujeong.K" />
+      <meta property="og:image" content="/images/backgroundImage.jpg" />
+      <meta property="og:url" content="https://ksj-frontend-dev.web.app/" />
+      <meta
+        property="og:description"
+        content="👋 안녕하세요, 프론트엔드 개발자 강수정입니다 :)"
+      />
+      <meta property="og:locale" content="ko_KR" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="Resume Sujeong.K" />
+      <meta name="twitter:image" content="/images/backgroundImage.jpg" />
+      <meta
+        name="twitter:description"
+        content="👋 안녕하세요, 프론트엔드 개발자 강수정입니다 :)"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout/Layout";
+import Seo from "@/components/Seo";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Seo />
       <Layout>
         <Component {...pageProps} />
       </Layout>
